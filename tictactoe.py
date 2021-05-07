8b927eb56aa67c49669fcbe09103a8c35e981f9f
mhemanthkmr <m.hemanthkmr143@gmail.com>
2021-05-07 18:15:31 +0530

    added a global variable and created a if statement check who is the winner

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,24 @@
-
+# --------- Global Variables -----------
+# Game Board
 board = ["-","-","-",
          "-","-","-",
          "-","-","-"]
+
+# If Game is still going
+
+game_still_going = True
+
+# Who won or tie ?
+
+Winner = None 
+
+# Who's Turn is it ?
+
+currentPlayer = "X"
+
+# --------- End of Global Variables ----------
+
+# Functions are Starting 
 
 def display_board():
     print(board[0] + " | " + board[1] + " | " + board[2])
@@ -12,13 +29,36 @@ def playGame() :
     # Display the initial Board 
     display_board() 
     while game_still_going :
-        handleTurn()
+        handleTurn(currentPlayer)
+        checkIfGameOver()
+        flipPlayer()
+    if Winner == "X" or Winner == "O" :
+        print(Winner + "Won")
+    elif Winner == None :
+        print("Tie !")
 
-def handleTurn():
+def handleTurn(player):
     position = input("Chose a Position from 1 to 9 : ")
     position = int(position) - 1 
 
     board[position] = "X" 
 
+    display_board()
+
+def checkIfGameOver():
+    checkIfWin()
+    checkIfDraw()
+
+def checkIfWin():
+    # Check rows
+    # Check Columns
+    # Check Diagonal
+    return
+
+def checkIfDraw():
+    return
+
+def flipPlayer():
+    return
 
 playGame()
