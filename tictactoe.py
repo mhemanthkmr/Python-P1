258c6e8fea55d1c111a3c680f339e564fae0f06c
mhemanthkmr <m.hemanthkmr143@gmail.com>
2021-05-07 17:59:33 +0530

    created a handle turn and playgame functions

--- tictactoe.py
+++ tictactoe.py
@@ -8,5 +8,17 @@ def display_board():
     print(board[3] + " | " + board[4] + " | " + board[5])
     print(board[6] + " | " + board[7] + " | " + board[8])
 
+def playGame() :
+    # Display the initial Board 
+    display_board() 
+    while game_still_going :
+        handleTurn()
 
-display_board()
+def handleTurn():
+    position = input("Chose a Position from 1 to 9 : ")
+    position = int(position) - 1 
+
+    board[position] = "X" 
+
+
+playGame()
